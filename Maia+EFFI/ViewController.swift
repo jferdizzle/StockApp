344961fb3fa33872602e0722898d2306d0d7f489
Julian Ferdman <jaf428@cornell.edu>
2016-10-27 17:15:57 -0400

    Quick fix on loading data on opening the app

--- Maia+EFFI/ViewController.swift
+++ Maia+EFFI/ViewController.swift
@@ -31,6 +31,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         
         loadTableView()
+        getStockData()
         view.backgroundColor = .yellow
         
     }
